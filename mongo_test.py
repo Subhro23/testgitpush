32c1b7243045f0a2702fb79aad7ba92632d8f3f4
subhro23 <subhramondal7777@gmail.com>
2023-07-30 23:20:43 +0530

    This is my first git code

---
+++ mongo_test.py
@@ -0,0 +1,21 @@
+import pymongo
+
+client= pymongo.MongoClient("mongodb+srv://subhramondal:subhra100@cluster0.yeaw9sz.mongodb.net/?retryWrites=true&w=majority")
+
+db=client.test
+print(db)
+
+d = {
+    "name" : "subhra",
+    "address" : "kolkata"
+}
+
+db2=client["mongo_test"]
+coll=db2["test"]
+coll.insert_one(d)
+
+
+
+
+
+
